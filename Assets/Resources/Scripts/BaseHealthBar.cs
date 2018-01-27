629660ae4d4aa387fc878e1045e1b2f3ac5c5bfe
mjeffery <michael.jeffery@gmail.com>
2018-01-27 12:52:52 -0500

    health bar for the player's base

---
+++ Assets/Resources/Scripts/BaseHealthBar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BaseHealthBar : MonoBehaviour {
+
+	private Image healthBar;
+
+	void Start () {
+		healthBar = GetComponent<Image> ();
+	}
+
+	public void OnHealthChanged(float health, float totalHealth) {
+		healthBar.fillAmount = health / totalHealth;
+	}
+}
---
+++ Assets/Resources/Scripts/HealthChangedEvent.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class HealthChangedEvent : UnityEvent<float, float> { } //TODO add a player identifier to this!
