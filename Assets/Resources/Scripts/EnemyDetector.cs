1ccd5de0c033c498275c2057fe90f0ef0befa3b3
Shafournee <31718336+Shafournee@users.noreply.github.com>
2018-01-27 13:42:06 -0500

    Towers shoot at enemies and stop when they leave

--- Assets/Resources/Scripts/EnemyDetector.cs
+++ Assets/Resources/Scripts/EnemyDetector.cs
@@ -18,17 +18,29 @@ public class EnemyDetector : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
+        //Check if the thing entering the trigger is on the enemy layer
         if (collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
-            //If the tower is not shooting, set it to start shooting
+            //If the tower is not shooting, set it to start shooting, this will stop it from exiting the shooting state when we don't want it to
             if(Tower.SwitchStates != Tower.State.StartShooting)
             {
                 Tower.SwitchStates = Tower.State.StartShooting;
+                Tower.EnemyBeingShot = collider.gameObject;
             }
         }
     }
 
-
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if(collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            //If the enemy leaving the trigger is the enemy being shot by the tower, change who the tower is shooting
+            if(collider.gameObject == Tower.EnemyBeingShot)
+            {
+                Tower.SwitchStates = Tower.State.FindNextTarget;
+            }
+        }
+    }
 
     private void DetectEnemies()
     {
