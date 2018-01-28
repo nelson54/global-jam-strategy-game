98ecbb17cfcef1c8ae2635504c1e4db457b59575
Shafournee <31718336+Shafournee@users.noreply.github.com>
2018-01-28 14:05:53 -0500

    Added boss spawn

---
+++ Assets/Resources/Scripts/BossSpawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpawn : Spawn {
+
+    public GameObject enemyPrefab;
+    public float hp;
+    public float interval;
+
+    public IEnumerator Run(EnemyPathNode start)
+    {
+        var instance = GameObject.Instantiate(enemyPrefab);
+        hp = instance.GetComponent<Enemy>().Health;
+        instance.transform.position = start.transform.position;
+        var follow = instance.GetComponent<FollowPathEnemy>();
+        follow.nextNode = start;
+        yield return new WaitForSeconds(interval);
+        
+    }
+}
