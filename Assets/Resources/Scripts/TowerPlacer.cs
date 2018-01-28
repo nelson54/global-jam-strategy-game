33b11d92a71abc6430385fa6c3a99c140cb76c97
Shafournee <31718336+Shafournee@users.noreply.github.com>
2018-01-28 02:06:31 -0500

    Added a tower placer

---
+++ Assets/Resources/Scripts/TowerPlacer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPlacer : Singleton<TowerPlacer> {
+
+    public List<GameObject> Towers;
+    GameObject TowerToPlace;
+    Transform PlacementPosition;
+
+	// Use this for initialization
+	void Start () {
+        Towers = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public GameObject PlaceATower(TowerType TypeOfTower, Color PlayerColor)
+    {
+        for(int i = 0; i < Towers.Count; i++)
+        {
+            if(Towers[i].GetComponent<Tower>().Type == TypeOfTower)
+            {
+                TowerToPlace = Towers[i];
+                break;
+            }
+        }
+        GameObject InstancedTower = Instantiate(TowerToPlace, PlacementPosition);
+        InstancedTower.GetComponent<SpriteRenderer>().color = PlayerColor;
+        return InstancedTower;
+    }
+
+
+}
