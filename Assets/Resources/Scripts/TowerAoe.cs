524e4a87798cd9f9ed5c9df7786d70571b7086d7
derek <dereknelson01@gmail.com>
2018-01-28 14:47:22 -0500

    Adding Aoe Effect

--- Assets/Resources/Scripts/TowerAoe.cs
+++ Assets/Resources/Scripts/TowerAoe.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TowerAoe : Tower {
 	public float missRate = .2f;
 	public float bulletDamage = 5f;
+	public GameObject effect;
 	public override void StartShooting()
     {
         Timer += Time.deltaTime;
@@ -22,6 +23,9 @@ public class TowerAoe : Tower {
         var EnemyList = Physics2D.OverlapCircleAll(transform.position, EnemyCircleDetector.GetComponent<CircleCollider2D>().radius, 1 << LayerMask.NameToLayer("Enemy"));
         if(EnemyList.Length > 0)
         {
+			GameObject instantiatedEffect = Instantiate<GameObject> (effect);
+			instantiatedEffect.transform.position = transform.position;
+
 			for (int i = 0; i < EnemyList.Length * (1f-missRate); i++)
             {
                 //If the enemy found in the list is not null start shooting at that
