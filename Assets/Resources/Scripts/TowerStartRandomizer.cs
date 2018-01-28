a6834062cc4aef49763da045ce0b546c0b6e6861
Shafournee <31718336+Shafournee@users.noreply.github.com>
2018-01-28 04:04:12 -0500

    Factories Spawn Randomly

---
+++ Assets/Resources/Scripts/TowerStartRandomizer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerStartRandomizer : Singleton<TowerStartRandomizer> {
+
+    public GameObject Factory1;
+    public GameObject Factory2;
+    public GameObject Factory3;
+    public List<GameObject> FactoryTypes;
+
+	// Use this for initialization
+	void Start () {
+        GameObject RandomFactory;
+        RandomFactory = FactoryTypes[Random.Range(0, FactoryTypes.Count)];
+        GameObject PlacedFactory1 = Instantiate(RandomFactory, Factory1.transform.position, Quaternion.identity);
+        RandomFactory = FactoryTypes[Random.Range(0, FactoryTypes.Count)];
+        GameObject PlacedFactory2 = Instantiate(RandomFactory, Factory2.transform.position, Quaternion.identity);
+        RandomFactory = FactoryTypes[Random.Range(0, FactoryTypes.Count)];
+        GameObject PlacedFactory3 = Instantiate(RandomFactory, Factory3.transform.position, Quaternion.identity);
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
